35d3a3dce687dedaca098c550d3188de51896431
Mikhail <mikhailbelov737@gmail.com>
2025-12-29 19:22:25 +0100

    feat: set inter font

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
 import { ThemeProvider } from '@/components/ThemeProvider';
 
 import './globals.css';
 
-const geistSans = Geist({
+const inter = Inter({
   variable: '--font-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-mono',
   subsets: ['latin'],
 });
 
@@ -26,11 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable}`}
-      suppressHydrationWarning
-    >
+    <html lang="en" className={`${inter.variable}`} suppressHydrationWarning>
       <body className={`antialiased`}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
